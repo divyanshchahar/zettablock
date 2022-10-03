0a2bee9a1d16167818b61d5a95e5df4fb6f020ed
divyanshchahar <divyanshchahar@outlook.com>
2022-10-03 10:59:49 +0530

    added boiler plate for FetchApiData.js

--- src/utils/FetchApiData.js
+++ src/utils/FetchApiData.js
@@ -1,21 +1,3 @@
-function FetchAPiData(params) {
-  fetch(params.url)
-    .then((res) => {
-      if (res.status !== 200) {
-        params.errorFunc("error! status code is not 200");
-        params.loadingFunc(false);
-      }
-      return res.json();
-    })
-    .then((data) => {
-      if (data.length > 0) {
-        params.loadingFunc(false);
-        params.stateFunc(data);
-      }
-    })
-    .catch((error) => {
-      params.errorFunc(`Error!${error}`);
-    });
-}
+function FetchAPiData(params) {}
 
 export default FetchAPiData;
