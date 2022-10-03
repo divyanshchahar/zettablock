a231c6684b65b78fe670cad156892dd5695f0eb0
divyanshchahar <divyanshchahar@outlook.com>
2022-10-03 23:36:09 +0530

    added functionality to handle errors

--- src/layouts/ApiList.js
+++ src/layouts/ApiList.js
@@ -1,12 +1,45 @@
 // IMPORTING FUNCTIONALITY
 import { useState } from "react";
+import { useEffect } from "react";
+
+// IMPORTING COMPONENETS
+import FetchAPiData from "../utils/FetchApiData";
+import ApiItem from "./ApiItem";
 
 function ApiLists(params) {
   const [apis, setApis] = useState();
   const [isError, setisError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  return <div></div>;
+  const url = params.props;
+  const propArgs = {
+    url: url,
+    loadingFunc: setIsLoading,
+    errorFunc: setisError,
+    stateFunc: setApis,
+  };
+
+  useEffect(() => {
+    FetchAPiData(propArgs);
+  }, []);
+
+  return (
+    <>
+      {/* HANDLING LOADING */}
+      {isLoading && (
+        <div className="loadingContainer">
+          <h1>Loading ...</h1>
+        </div>
+      )}
+      {/* HANDLING ERROR */}
+      {isError && (
+        <div className="errorContainer">
+          <h1>Error</h1>
+          <p>{isError}</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ApiLists;
