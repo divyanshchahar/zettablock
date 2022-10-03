9087d44c07aaa934f7f53d5f952e1db6870ae76b
divyanshchahar <divyanshchahar@outlook.com>
2022-10-03 21:44:37 +0530

    added functionality to display api name

--- src/layouts/ApiItem.js
+++ src/layouts/ApiItem.js
@@ -1,7 +1,12 @@
-import React from "react";
-
-function ApiItem() {
-  return <div></div>;
+function ApiItem(params) {
+  return (
+    <>
+      {/* API NAME */}
+      <div>
+        <h1>{params.props.name}</h1>
+      </div>
+    </>
+  );
 }
 
 export default ApiItem;
