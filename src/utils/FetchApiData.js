ff0d0d36b5b375e656dc976b05180acfc8bae1ff
divyanshchahar <divyanshchahar@outlook.com>
2022-10-03 11:03:17 +0530

    changed FetchAPiData.js - added functionality to handle status code

--- src/utils/FetchApiData.js
+++ src/utils/FetchApiData.js
@@ -1,3 +1,10 @@
-function FetchAPiData(params) {}
+function FetchAPiData(params) {
+  fetch(params.url).then((res) => {
+    if (res.status !== 200) {
+      params.errorFunc("error! status code is not 200");
+      params.loadingFunc(false);
+    }
+  });
+}
 
 export default FetchAPiData;
