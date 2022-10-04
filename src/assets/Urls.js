a74eae1e67ec15b33c45da1007b69c2202d5d730
divyanshchahar <divyanshchahar@outlook.com>
2022-10-04 19:46:03 +0530

    changed Urls.js: - added strigs to enable pagination - changed strings for asc and dsc arguments

--- src/assets/Urls.js
+++ src/assets/Urls.js
@@ -1,7 +1,12 @@
 const Urls = {
   baseApi: "https://6339a12066857f698fb934c2.mockapi.io/testingAPi/fakeApi",
-  asc: "https://6339a12066857f698fb934c2.mockapi.io/testingAPi/fakeApi/?sortBy=name&order=asc",
-  dsc: "https://6339a12066857f698fb934c2.mockapi.io/testingAPi/fakeApi/?sortBy=name&order=desc",
+  asc: "?sortBy=name&order=asc",
+  dsc: "?sortBy=name&order=desc",
+  p1: "?page=1&limit=10",
+  p2: "?page=2&limit=20",
+  p3: "?page=3&limit=30",
+  p4: "?page=4&limit=40",
+  p5: "?page=5&limit=50",
 };
 
 export default Urls;
