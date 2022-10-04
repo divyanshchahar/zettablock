be6b3a93b89a9736fe81644eefe5d9ea71ff0d6e
divyanshchahar <divyanshchahar@outlook.com>
2022-10-04 18:47:05 +0530

    added functionality to make DELETE request

--- src/layouts/ApiItem.js
+++ src/layouts/ApiItem.js
@@ -1,13 +1,19 @@
 // IMPORTING FUNCTIONALITY
 import { useState } from "react";
 
+//IMPORTING COMPONENTS
+import DeleteApiData from "../utils/DeleteApiData";
+
+//IMPORTING ASSETS
+import Urls from "../assets/Urls";
+
 function ApiItem(params) {
   const items = Object.entries(params.props.apiItem);
   const [isVisible, setIsVisible] = useState(false);
 
   return (
     <>
-      {/* API NAME */}
+      {/* API NAME AND DELETE BUTTON*/}
       <div
         onClick={() => {
           params.props.hidingFunc(setIsVisible);
@@ -15,6 +21,13 @@ function ApiItem(params) {
       >
         <h1>{params.props.name}</h1>
       </div>
+      <button
+        onClick={() => {
+          DeleteApiData(`${Urls.baseApi}/${params.props.apiItem.id}`);
+        }}
+      >
+        Delete
+      </button>
 
       {/* TABLE */}
       {isVisible && (
