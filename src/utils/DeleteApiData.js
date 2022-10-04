85f3b07d046e52a1fe80e8c7234331d4522eedd8
divyanshchahar <divyanshchahar@outlook.com>
2022-10-04 19:16:30 +0530

    made the following changes: - cropped image of downward arrow - removed un necessary files

--- src/utils/DeleteApiData.js
+++ src/utils/DeleteApiData.js
@@ -3,7 +3,7 @@ function DeleteApiData(params) {
   fetch(params, {
     method: "DELETE",
     headers: { "content-type": "application/json" },
-  });
+  }).then((res) => console.log(res));
 }
 
 export default DeleteApiData;
