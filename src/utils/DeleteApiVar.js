9545c8fff2c8595ac46d6eb52c22fba07ee716af
divyanshchahar <divyanshchahar@outlook.com>
2022-10-05 16:13:54 +0530

    added code to delete api items

--- src/utils/DeleteApiVar.js
+++ src/utils/DeleteApiVar.js
@@ -1,5 +1,9 @@
-function DeleteApiVar() {
-  return <div></div>;
+function DeleteApiVar(params) {
+  let newData = params.props.stateVar.filter((item) => {
+    return item.id !== params.props.id;
+  });
+
+  params.props.stateFunc(newData);
 }
 
 export default DeleteApiVar;
