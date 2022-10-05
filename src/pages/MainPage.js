0a05ad68887dff7cc45a05c4ac7a5ae6394d67ed
divyanshchahar <divyanshchahar@outlook.com>
2022-10-05 14:13:03 +0530

    added functionality to handle pagination: - added callback to change pagination - added arguments objects for changing pagination

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,22 +4,31 @@ import { useState } from "react";
 //IMPORTING COMPONENTS
 import ApiList from "../layouts/ApiList";
 import SortingMenu from "../layouts/SortingMenu";
+import Pagination from "../layouts/Pagination";
 
 //IMPORTING ASSETS
 import Urls from "../assets/Urls";
 
 function MainPage() {
   const [sorting, setSorting] = useState(Urls.baseApi);
-  const propArgs = { sortingFunc: changeSorting, sortOptions: Urls };
+  const [page, setPage] = useState(Urls.baseApi);
 
   function changeSorting(param) {
     setSorting(param);
   }
 
+  function changePage(param) {
+    setPage(param);
+  }
+
+  const sortingPropArgs = { sortingFunc: changeSorting, sortOptions: Urls };
+  const pagePropArgs = { pageFunc: changePage, pageOptions: Urls };
+
   return (
     <>
-      <SortingMenu props={propArgs} />
+      <SortingMenu props={sortingPropArgs} />
       <ApiList props={sorting} />
+      <Pagination props={pagePropArgs} />
     </>
   );
 }
