d0d938f0a3bd2b2f766a3d1a1fe86b7aa322416b
divyanshchahar <divyanshchahar@outlook.com>
2022-10-05 14:29:42 +0530

    added functionality to chnage pagination on click

--- src/layouts/Pagination.js
+++ src/layouts/Pagination.js
@@ -2,13 +2,70 @@
 import Urls from "../assets/Urls";
 
 function Pagination(params) {
+  function handleClick(param) {
+    switch (param) {
+      case "1":
+        params.props.changePage(Urls.p1);
+        break;
+
+      case "2":
+        params.props.changePage(Urls.p2);
+        break;
+
+      case "3":
+        params.props.changePage(Urls.p3);
+        break;
+
+      case "4":
+        params.props.changePage(Urls.p4);
+        break;
+
+      case "5":
+        params.props.changePage(Urls.p5);
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
+      <button
+        onClick={() => {
+          handleClick("1");
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          handleClick("2");
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          handleClick("3");
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={() => {
+          handleClick("4");
+        }}
+      >
+        4
+      </button>
+      <button
+        onClick={() => {
+          handleClick("5");
+        }}
+      >
+        5
+      </button>
     </div>
   );
 }
