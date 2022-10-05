e4cac60dc82715b39b305f5bef97d52cc84a60fc
divyanshchahar <divyanshchahar@outlook.com>
2022-10-05 15:12:00 +0530

    fixed bug: - chnaged spelling

--- src/assets/Urls.js
+++ src/assets/Urls.js
@@ -1,13 +1,13 @@
 const Urls = {
-  baseApi: "https://6339a12066857f698fb934c2.mockapi.io/testingAPi/fakeApi",
-  noSrot: null,
-  asc: "?sortBy=name&order=asc",
-  dsc: "?sortBy=name&order=desc",
-  p1: "?page=1&limit=10",
-  p2: "?page=2&limit=20",
-  p3: "?page=3&limit=30",
-  p4: "?page=4&limit=40",
-  p5: "?page=5&limit=50",
+  baseApi: "https://6339a12066857f698fb934c2.mockapi.io/testingAPi/",
+  noSort: "fakeApi",
+  asc: "fakeApi/?sortBy=name&order=asc",
+  dsc: "fakeApi/?sortBy=name&order=desc",
+  p1: "/?page=1&limit=10",
+  p2: "/?page=2&limit=20",
+  p3: "/?page=3&limit=30",
+  p4: "/?page=4&limit=40",
+  p5: "/?page=5&limit=50",
 };
 
 export default Urls;
