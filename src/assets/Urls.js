d81048946b9ccc5be4b3a0453dbef161c08e9fa9
divyanshchahar <divyanshchahar@outlook.com>
2022-10-05 15:24:22 +0530

    fixed bug - changed limit

--- src/assets/Urls.js
+++ src/assets/Urls.js
@@ -4,10 +4,10 @@ const Urls = {
   asc: "fakeApi/?sortBy=name&order=asc",
   dsc: "fakeApi/?sortBy=name&order=desc",
   p1: "/?page=1&limit=10",
-  p2: "/?page=2&limit=20",
-  p3: "/?page=3&limit=30",
-  p4: "/?page=4&limit=40",
-  p5: "/?page=5&limit=50",
+  p2: "/?page=2&limit=10",
+  p3: "/?page=3&limit=10",
+  p4: "/?page=4&limit=10",
+  p5: "/?page=5&limit=10",
 };
 
 export default Urls;
